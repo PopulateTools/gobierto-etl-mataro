ea0728535b2e672bd9c822794e7dd85acc2b470a
Eduardo Martínez <eduardomech@gmail.com>
2019-10-23 17:25:07 +0200

    Add development and jenkins pipelines of investments_projects ETL

---
+++ pipelines/investments_projects/dev_run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+source "/Users/edu/.rvm/scripts/rvm"
+set -e
+
+WORKING_DIR=/tmp/mataro
+MATARO_INE_CODE=8121
+API_HOST=http://mataro.gobierto.test
+ATTACHMENTS_COLLECTION_ID=2026
+
+# Clean working dir
+rm -rf $WORKING_DIR
+mkdir $WORKING_DIR
+
+# Extract > Download data sources - Projects index
+cd $DEV_DIR/gobierto-etl-utils/; ruby operations/download/run.rb "https://aplicacions.mataro.org:444/apex/rest/sigmav2/llistaobres2" $WORKING_DIR/llistaobres.json
+
+# Extract > Download data sources - API resources
+cd $DEV_DIR/gobierto-etl-utils/; ruby operations/download/run.rb "$API_HOST/gobierto_investments/api/v1/projects/meta" $WORKING_DIR/meta.json
+cd $DEV_DIR/gobierto-etl-utils/; ruby operations/download/run.rb "$API_HOST/gobierto_investments/api/v1/projects/new" $WORKING_DIR/new.json
+
+# Extract > Extract external ids of projects
+cd $DEV_DIR/gobierto-etl-mataro/; ruby operations/gobierto_investments/extract-projects-external-ids/run.rb $WORKING_DIR/llistaobres.json $WORKING_DIR/external_ids.txt
+
+# Extract > Download data sources - Individual projects
+for i in $(cat $WORKING_DIR/external_ids.txt) ; do
+  cd $DEV_DIR/gobierto-etl-utils/; ruby operations/download/run.rb "https://aplicacions.mataro.org:444/apex/rest/sigmav2/detallobre2/$i" $WORKING_DIR/downloaded_projects/$i.json
+done
+
+# Transform > Transform data
+cd $DEV_DIR/gobierto-etl-mataro/; ruby operations/gobierto_investments/transform-projects/run.rb $WORKING_DIR/external_ids.txt $WORKING_DIR/meta.json $WORKING_DIR/new.json $WORKING_DIR/downloaded_projects/ $WORKING_DIR/transformed_projects/ $API_HOST $ATTACHMENTS_COLLECTION_ID
+
+# Load > Send data to API
+cd $DEV_DIR/gobierto-etl-mataro/; ruby operations/gobierto_investments/load-projects/run.rb $WORKING_DIR/external_ids.txt $WORKING_DIR/transformed_projects/ $API_HOST
+
+# Load > Publish activity
+echo $MATARO_INE_CODE > $WORKING_DIR/organization.id.txt
+cd $DEV_DIR/gobierto/; bin/rails runner $DEV_DIR/gobierto-etl-utils/operations/gobierto/publish-activity/run.rb investments_projects_updated $WORKING_DIR/organization.id.txt
