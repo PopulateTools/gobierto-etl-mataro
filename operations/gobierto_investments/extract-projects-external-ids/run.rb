d179e583e6ca32bbd978412793264335691f27e9
Eduardo Martínez <eduardomech@gmail.com>
2019-11-25 23:23:18 +0100

    Change extract-projects-external-ids operation to generate extra_data json file

--- operations/gobierto_investments/extract-projects-external-ids/run.rb
+++ operations/gobierto_investments/extract-projects-external-ids/run.rb
@@ -10,29 +10,46 @@ Bundler.require
 # Arguments:
 #
 #  - 0: Absolute path to the input file
-#  - 1: Absolute path to the output file
+#  - 1: Absolute path to the output file with the list of ids to send
+#       indivicual requests
+#  - 2: Absoulte path to the output file with a json containing data of each
+#       project not present in individual requests
 #
 # Samples:
 #
-#   /path/to/project/operations/gobierto_budgets/extract-projects-external-ids/run.rb input.json output.json
+#   /path/to/project/operations/gobierto_budgets/extract-projects-external-ids-and-extra-data/run.rb input.json external_ids.txt data.json
 #
 
-if ARGV.length != 2
+if ARGV.length != 3
   raise "Review the arguments"
 end
 
 input_file = ARGV[0]
-output_file = ARGV[1]
+output_ids_file = ARGV[1]
+output_data_file = ARGV[2]
 
-puts "[START] extract-projects-external-ids/run.rb with input_file=#{input_file} and output_file=#{output_file}"
+puts "[START] extract-projects-external-ids-and-extra-data/run.rb with input_file=#{input_file} output_ids_file=#{output_ids_file} and output_data_file=#{output_data_file}"
 
 input = File.open(input_file).read
-ids = JSON.parse(input)["items"][0]["llistaobres2"].map{ |item| item["id"] }
+parsed_data = JSON.parse(input)["items"][0]["llistaobres2"]
+ids = parsed_data.map { |item| item["id"] }
 
-if File.dirname(output_file) != "."
-  FileUtils.mkdir_p(File.dirname(output_file))
+data = parsed_data.inject({}) do |hsh, item|
+  hsh.update(
+    item["id"] => item.slice("element", "import", "tipus")
+  )
 end
 
-File.write(output_file, ids.join(" "))
+if File.dirname(output_ids_file) != "."
+  FileUtils.mkdir_p(File.dirname(output_ids_file))
+end
 
-puts "[END] extract-project-external-ids/run.rb"
+File.write(output_ids_file, ids.join(" "))
+
+if File.dirname(output_data_file) != "."
+  FileUtils.mkdir_p(File.dirname(output_data_file))
+end
+
+File.write(output_data_file, data.to_json)
+
+puts "[END] extract-projects-external-ids-and-extra-data/run.rb"
