64c4204027829d14ed32c092e683e0bd6635420b
Alberto Miedes <albertomg994@gmail.com>
2020-01-15 16:44:43 +0100

    Fix skipped rows

--- operations/gobierto_budgets/transform-planned/run.rb
+++ operations/gobierto_budgets/transform-planned/run.rb
@@ -56,7 +56,12 @@ end
 
 def parse_cell(row, year, name)
   #return if row['PARANYPRS'].to_i != year
-  return if row['IMPASSIG'].blank?
+
+  return if year == 2020 && row["IMPASSIG_V2"].blank?
+  return if year != 2020 && row["IMPASSIG_V1"].blank?
+
+  #return if row['IMPASSIG'].blank?
+
   return if row[name].blank?
   category_name = row[name].strip
 
