9003c829db23477eda88f472e6f22438e0e7839a
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2026-02-11 12:45:12 +0100

    Update dev pipeline and change mataro API endpoints

--- pipelines/plans/dev_run.sh
+++ pipelines/plans/dev_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-source "/Users/edu/.rvm/scripts/rvm"
+source "/home/edu/.rvm/scripts/rvm"
 set -e
 
 WORKING_DIR=/tmp/mataro_plans
 MATARO_INE_CODE=8121
 API_HOST=http://madrid.gobierto.test:3000
-PAM_PLAN_ID=57
-URBAN_AGENDA_2030_PLAN_ID=58
+PAM_PLAN_ID=63
+URBAN_AGENDA_2030_PLAN_ID=64
 API_TOKEN=LUhwRCoBENVvzLUJMcuYbXkg
 
 # Clean working dir
@@ -14,7 +14,7 @@ rm -rf $WORKING_DIR
 mkdir $WORKING_DIR
 
 # [PAM] Extract > Download data sources - Plan data
-cd $DEV_DIR/gobierto-etl-utils/; ruby operations/api-download/run.rb --source-url "https://aplicacions.mataro.org:444/apex/rest/sigmav2/getjson/SIGMA/SEGUIMENT/id_plan/201872" --output-file $WORKING_DIR/PAM_source_data.json
+cd $DEV_DIR/gobierto-etl-utils/; ruby operations/api-download/run.rb --source-url "https://apex.mataro.org/ords/rest/sigmav2/getjson/SIGMA/SEGUIMENT/id_plan/201872" --output-file $WORKING_DIR/PAM_source_data.json
 
 # [PAM] Transform > Transform data
 cd $DEV_DIR/gobierto-etl-mataro/; ruby operations/gobierto_plans/transform-projects/run.rb $WORKING_DIR/PAM_source_data.json $WORKING_DIR/PAM_request_body.json PAM
@@ -23,7 +23,7 @@ cd $DEV_DIR/gobierto-etl-mataro/; ruby operations/gobierto_plans/transform-proje
 cd $DEV_DIR/gobierto-etl-mataro/; ruby operations/gobierto_plans/upsert-projects/run.rb $WORKING_DIR/PAM_request_body.json $API_HOST/api/v1/plans/$PAM_PLAN_ID
 
 # [Urban Agenda 2030] Extract > Download data sources - Plan data
-cd $DEV_DIR/gobierto-etl-utils/; ruby operations/api-download/run.rb --source-url "https://aplicacions.mataro.org:444/apex/rest/sigmav2/getjson/SIGMA/SEGUIMENT/id_plan/253522" --output-file $WORKING_DIR/urban_agenda_2030_source_data.json
+cd $DEV_DIR/gobierto-etl-utils/; ruby operations/api-download/run.rb --source-url "https://apex.mataro.org/ords/rest/sigmav2/getjson/SIGMA/SEGUIMENT/id_plan/253522" --output-file $WORKING_DIR/urban_agenda_2030_source_data.json
 
 # [Urban Agenda 2030] Transform > Transform data
 cd $DEV_DIR/gobierto-etl-mataro/; ruby operations/gobierto_plans/transform-projects/run.rb $WORKING_DIR/urban_agenda_2030_source_data.json $WORKING_DIR/urban_agenda_2030_request_body.json urban_agenda_2030
